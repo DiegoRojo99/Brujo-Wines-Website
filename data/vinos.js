b7970f4778d6d89674b165bef5588a5c663e98ce
DiegoRojo99 <diego99rojo@gmail.com>
2022-06-29 11:55:07 +0200

    Vinos y admin

---
+++ data/vinos.js
@@ -0,0 +1,21 @@
+const vinos = {
+    Vino1: {
+        nombre: 'Titania Blanco',
+        tipo:'blanco',
+        imagen: 'TitaniaBlanco.png',
+        precio: 30
+    },
+    Vino2: {
+        nombre: 'Titania Tinto',
+        tipo:'tinto',
+        imagen: 'TitaniaTinto.png',
+        precio: 40
+    },
+    Vino3: {
+        nombre: 'Titania Rosado',
+        tipo:'rosado',
+        imagen: 'TitaniaRosado.png',
+        precio: 25
+    }
+}
+
--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,43 @@
+
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+import { getFirestore, addDoc,  doc, getDoc, getDocs, collection } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-auth.js";
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth();
+let userId="";
+let admin=false;
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+    userId = user.uid;
+    } 
+});
+
+async function getAdmin() {
+    // Get a list of cities from your database 
+        
+        var admins=[];
+        var arrayPedidosID=[];
+        const adminCol = collection(db, 'admin');
+        const adminSnapshot = await getDocs(adminCol);
+        
+        adminSnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            if(doc.data().userId===userId){
+                addAdmin();
+            }
+        });
+
+        return 0;
+    }
+
+    getAdmin();
+
+
 //Si el usuario es admin añadir una pagina de admin a nav
 function addAdmin(){
     var adminLi = document.createElement("li");
