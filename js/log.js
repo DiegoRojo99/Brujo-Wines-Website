e7b88ae5f7ea38c996fb90ce583c285a8277ef88
DiegoRojo99 <diego99rojo@gmail.com>
2022-07-05 16:26:32 +0200

    Metodo registrar añadido

--- js/log.js
+++ js/log.js
@@ -24,12 +24,33 @@ const authenticate = (email, password) => {
     .then((userCredential) => {
       // Signed in
       const user = userCredential.user;
-      console.log(user);
-      // ...
     })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
       alert(errorMessage);
     });
+};
+
+const register = () => {
+    const email = document.querySelector("#registration-email").value;
+    const reemail = document.querySelector("#registration-reemail").value;
+    const password = document.querySelector("#registration-password").value;
+
+    if (email.trim() == "") {
+    alert("Introduzca el email");
+    } else if (password.trim().length < 6) {
+    alert("La contraseña tiene que ser de al menos 6 caracteres");
+    } else if (email != reemail) {
+    alert("Los correos no coinciden");
+    } else {
+        createUserWithEmailAndPassword(auth, email, password)
+        .catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert(errorMessage);
+        // ...
+        });
+    }
 };
