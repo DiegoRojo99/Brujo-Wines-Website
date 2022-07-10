43044f3ac5adfed5b9bc185f6dcf442554855d2d
DiegoRojo99 <diego99rojo@gmail.com>
2022-07-10 11:11:55 +0200

    Añadido el auth provider de facebook

---
+++ js/facebook.js
@@ -0,0 +1,31 @@
+import { signInWithPopup, FacebookAuthProvider } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-auth.js";
+import { auth} from './db.js';
+
+
+const provider = new FacebookAuthProvider();
+provider.addScope('email');
+
+signInWithPopup(auth, provider)
+  .then((result) => {
+    // The signed-in user info.
+    const user = result.user;
+
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    const credential = FacebookAuthProvider.credentialFromResult(result);
+    const accessToken = credential.accessToken;
+
+    // ...
+  })
+  .catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = FacebookAuthProvider.credentialFromError(error);
+
+    // ...
+  });
+
+console.log(provider);
