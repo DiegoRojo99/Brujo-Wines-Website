493fda2a44f8c62db2ea095f6781609e44b409c0
DiegoRojo99 <diego99rojo@gmail.com>
2022-07-10 12:55:24 +0200

    Añadida funcion login con Google

--- js/facebook.js
+++ js/facebook.js
@@ -62,19 +62,32 @@ function entrarFacebook(){
 }
 
 function entrarGoogle(){
-    linkWithPopup(auth.currentUser, provider).then((result) => {
-        // Accounts successfully linked.
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
         const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
         const user = result.user;
         // ...
-    }).catch((error) => {
+      }).catch((error) => {
         // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
         // ...
-    });
+      });
 }
 
 document.querySelector("#facebook-login").addEventListener("click", () => {
     entrarFacebook();
+});
+
+document.querySelector("#google-login").addEventListener("click", () => {
+    entrarGoogle();
 });
 
 function goToApp(){
