74e0c43cc294d9aeb495d3071159801aa90e156d
DiegoRojo99 <diego99rojo@gmail.com>
2022-07-10 11:53:22 +0200

    Metodo Sign in solo al pulsar

--- js/facebook.js
+++ js/facebook.js
@@ -7,8 +7,10 @@ provider.setCustomParameters({
     'display': 'popup'
   });
 
-signInWithPopup(auth, provider)
-  .then((result) => {
+function entrarFacebook(){
+    
+    signInWithPopup(auth, provider)
+    .then((result) => {
     // The signed-in user info.
     const user = result.user;
 
@@ -17,8 +19,8 @@ signInWithPopup(auth, provider)
     const accessToken = credential.accessToken;
 
     // ...
-  })
-  .catch((error) => {
+    })
+    .catch((error) => {
     if (error.code === 'auth/account-exists-with-different-credential') {
 
         console.log("ERROR ENCONTRADO");
@@ -29,36 +31,38 @@ signInWithPopup(auth, provider)
         // Get sign-in methods for this email.
         auth.fetchSignInMethodsForEmail(email).then(function(methods) {
             
-          if (methods[0] === 'password') {
+            if (methods[0] === 'password') {
             // Asks the user their password.
             // In real scenario, you should handle this asynchronously.
             var password = promptUserForPassword(); // TODO: implement promptUserForPassword.
             auth.signInWithEmailAndPassword(email, password).then(function(result) {
-              // Step 4a.
-              return result.user.linkWithCredential(pendingCred);
+                // Step 4a.
+                return result.user.linkWithCredential(pendingCred);
             }).then(function() {
-              // Facebook account successfully linked to the existing Firebase user.
-              goToApp();
+                // Facebook account successfully linked to the existing Firebase user.
+                goToApp();
             });
             return;
-          }
-          
-          var provider = getProviderForProviderId(methods[0]);
-          auth.signInWithPopup(provider).then(function(result) {
+            }
+            
+            var provider = getProviderForProviderId(methods[0]);
+            auth.signInWithPopup(provider).then(function(result) {
             
             result.user.linkAndRetrieveDataWithCredential(pendingCred).then(function(usercred) {
-              // Facebook account successfully linked to the existing Firebase user.
-              goToApp();
+                // Facebook account successfully linked to the existing Firebase user.
+                goToApp();
             });
-          });
+            });
         });
-      }else{
+        }else{
         console.log("ERROR SIN DEFINIR");
-      }
-  });
+        }
+    });
+}
+
 
 document.querySelector("#facebook-login").addEventListener("click", () => {
-    signInWithPopup(auth, provider);
+    entrarFacebook();
 });
 
 function goToApp(){
